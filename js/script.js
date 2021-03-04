8207afbe5661e474b17dbe9ea9d19776ae55bdb3
Robert Hysa <robert.hysa@wp.pl>
2021-03-04 21:09:49 +0100

    add function calculateTagsParams

--- js/script.js
+++ js/script.js
@@ -116,6 +116,20 @@ function generateTitleLinks(customSelector = ''){
 
 generateTitleLinks();
 
+function calculateTagsParams(tags){
+  const params = {'max': 0, 'min': 999999};
+  for(let tag in tags){
+    console.log(tag + ' is used ' + tags[tag] + ' times');
+    if(tags[tag] > params.max){
+      params.max = tags[tag];
+    }
+    if(tags[tag] < params.min){
+      params.min = tags[tag];
+    }
+  }
+  return params;
+}
+
 function generateTags(){
   /* [NEW] create a new variable allTags with an empty object */
 
@@ -192,6 +206,9 @@ function generateTags(){
   /* [NEW] find list of tags in right column */
 
   const tagList = document.querySelector('.tags');
+
+  const tagsParams = calculateTagsParams(allTags);
+  console.log('tagsParams:', tagsParams);
 
   /* [NEW] create variable for all links HTML code */
 
